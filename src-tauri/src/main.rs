7ff0aa987fa600674f8de739d8fc964f8e57b420
lsx <123@123>
2022-07-16 21:58:41 +0800

    添加毛玻璃效果

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,9 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+mod setup;
 
 fn main() {
+  let context = tauri::generate_context!();
   tauri::Builder::default()
-    .run(tauri::generate_context!())
+    .setup(setup::init)
+    .run(context)
     .expect("error while running tauri application");
 }
